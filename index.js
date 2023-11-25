413119d0d13b9fc934d7ea92ee25365e9c854caf
mursalinmirme <mursalinmir02@gmail.com>
2023-11-26 01:01:24 +0600

    I have make a users route which used for post an user information in database

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
 const express = require('express');
 const app = express();
 require('dotenv').config()
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
+
+
 app.get('/', (req, res) => {
     res.send('The Wellnex server is running.....');
 })
 
+console.log(process.env.USER_NAME);
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@mursalin.bxh3q56.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const usersCollection = client.db('wellnex').collection('users');
+
+    // insert users registration data
+    app.post('/users', async(req, res) => {
+        const usersData = req.body;
+        const inserUser = await usersCollection.insertOne(usersData);
+
+        console.log(inserUser);
+    })
+
+
+
+    client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
