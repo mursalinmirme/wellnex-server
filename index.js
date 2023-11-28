60616962469de427ebca8238e399f9f24ed1d2c7
mursalinmirme <mursalinmir02@gmail.com>
2023-11-28 22:12:19 +0600

    I have make a route for get a perticipant all of his payment history

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 const cors = require('cors');
+const stripe = require("stripe")(process.env.STRIP_SECRET_API);
 const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
@@ -32,6 +33,7 @@ async function run() {
     const participantRatingsCollection = client.db('wellnex').collection('participant_ratings')
     const wellnessBlogsCollection = client.db('wellnex').collection('wellness_blogs');
     const joinCampRegCollection = client.db('wellnex').collection('join_camp_registrations');
+    const paymentCollection = client.db('wellnex').collection('payments');
 
     // insert users registration data
     app.post('/users', async(req, res) => {
@@ -105,8 +107,8 @@ async function run() {
       res.send({userRole: findUser?.role});
     })
 
-    // organizer data get
-    app.get('/organizer', async(req, res) => {
+    // get users details
+    app.get('/users', async(req, res) => {
       const query = req.query;
       const result = await usersCollection.findOne(query);
       res.send(result)
@@ -155,7 +157,7 @@ async function run() {
       console.log(result);
       res.send(result);
     })
-
+    // update camps
     app.put('/organizers-camps/:id', async(req, res) => {
       const updateId = req.params;
       const updateInfo = req.body;
@@ -167,6 +169,99 @@ async function run() {
       }
       const result = await campsCollection.updateOne(filter, updateDoc, option)
       res.send(result)
+    })
+
+    // get all registration under a organizer
+    app.get(`/registered_under_organizers`, async(req, res) => {
+      const organizerEmail = req.query?.email;
+      // console.log('orga ni zer is', organizerEmail);
+      const option = {
+        sort: {_id: 1},
+        projection: { _id:1, 'campInfo.camp_name':1, 'campInfo.scheduled_date_time':1, 'campInfo.venue_location':1,'campInfo.camp_fees':1, payment_status:1, confirmation_stauts:1, }
+      }
+      const result = await joinCampRegCollection.find({'campInfo.camp_owner': organizerEmail}, option).toArray();
+      // console.log('Mr Rahat got', result);
+      res.send(result);
+    })
+
+    // confiremed participant registrantion update
+    app.patch('/payment-status/:id', async(req, res) => {
+      const regId = req.params.id;
+      const result = await joinCampRegCollection.findOneAndUpdate({_id: new ObjectId(regId)}, {$set: {confirmation_stauts:'Confirmed'}}, {new: true});
+      console.log(result);
+      if(result){
+        res.send({operatonStatus: 'success'})
+      }
+    })
+
+    // organizers can delete participant registration
+    app.delete('/participant-register-camp/:id', async(req, res) => {
+      const deleteId = req.params;
+      const result = await joinCampRegCollection.deleteOne({_id: new ObjectId(deleteId.id)});
+      res.send(result);
+    })
+
+    // participant dahboard related routes
+    app.get(`/participant-registered-camps`, async(req, res) => {
+      const participantEmail = req.query?.email;
+      // console.log('orga ni zer is', organizerEmail);
+      const option = {
+        sort: {_id: 1},
+        projection: { _id:1, 'campInfo.camp_name':1, 'campInfo.scheduled_date_time':1, 'campInfo.venue_location':1,'campInfo.camp_fees':1, payment_status:1, confirmation_stauts:1, }
+      }
+      const result = await joinCampRegCollection.find({participantEmail: participantEmail}, option).toArray();
+      // console.log('Mr Rahat got', result);
+      res.send(result);
+    })
+
+    // stripe payment intregation here
+    app.post("/create-payment-intent", async (req, res) => {
+      const {price}  = req.body;
+      // Create a PaymentIntent with the order amount and currency
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: parseInt(price * 100),
+        currency: "usd",
+        payment_method_types: ['card']
+      });
+    
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
+
+    // payment card details get
+    app.get('/cart-camp-details/:id', async(req, res) => {
+      const detilsId = req.params.id;
+      // console.log(detilsId);
+      const result = await joinCampRegCollection.findOne({_id: new ObjectId(detilsId)});
+      // console.log(result);
+      res.send(result);
+    })
+
+    app.post('/payments', async(req, res) => {
+      const paymentDesc = req.body;
+      const result = await paymentCollection.insertOne(paymentDesc);
+      res.send(result);
+    })
+
+    app.patch('/update_reg_camps_payment_status/:id', async(req, res) => {
+      const updateId = req.params;
+      const result = await joinCampRegCollection.findOneAndUpdate({_id: new ObjectId(updateId?.id)}, {$set: {payment_status:'paid'}}, {new: true});
+      if(result){
+        res.send({updateSuccessfully: 'success'})
+      }
+    })
+
+    // get perticipant payment list
+    app.get('/participant-payment-history', async(req, res) => {
+        const partiEmail = req.query;
+        const option = {
+          sort: {_id: 1},
+          projection: { _id:1, camp_name:1, scheduled_date_time:1, venue_location:1,camp_fees:1, payment_status:1, confirmation:1, }
+        }
+        const result = await paymentCollection.find({paymentUser: partiEmail?.email}).toArray();
+        res.send(result);
     })
 
 
